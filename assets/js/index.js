4c208f9d4aa00d213a0e7b3e703a4c7092cfa64f
Łukasz Pikor <pikorlukasz@gmail.com>
2023-04-02 19:59:46 +0200

    Add header

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+const handleMenuDropdown = () => {
+    const pageHeader = document.querySelector('.page-header');
+    const menuTriggers = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item > a');
+    const menuItemParent = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item-has-children > a');
+
+    menuItemParent.forEach(parent => {
+        const subMenu = parent.parentNode.querySelector('.sub-menu');
+
+        parent.addEventListener('mouseenter', () => {
+            subMenu.classList.add('active');
+        });
+    });
+
+    menuTriggers.forEach(trigger => {
+        trigger.addEventListener('mouseenter', (e) => {
+            const subMenuActive = document.querySelector('.sub-menu.active');
+            if (!e.target.parentNode.querySelector('.sub-menu.active') && subMenuActive) {
+                subMenuActive.classList.remove('active');
+            }
+        });
+    });
+
+    pageHeader.addEventListener('mouseleave', () => {
+        console.log('test');
+        const subMenuActive = document.querySelector('.sub-menu.active');
+        if (subMenuActive) {
+            subMenuActive.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    handleMenuDropdown();
+});
