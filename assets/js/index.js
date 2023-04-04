d5629c3a643648d1058adba4ab9a962a34b8c798
Łukasz Pikor <pikorlukasz@gmail.com>
2023-04-04 20:25:53 +0200

    Refactor notification bar script

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,10 @@ import "../css/style.scss";
 
 import menu from "./menu";
 import scroll from "./scroll";
-import notificationBar from "./notification-bar";
+import NotificationBar from "./notification-bar";
 
 window.addEventListener('DOMContentLoaded', () => {
     menu();
-    notificationBar();
+    new NotificationBar().init();
     scroll();
 });
--- assets/js/notification-bar.js
+++ assets/js/notification-bar.js
@@ -1,37 +1,40 @@
-const notificationBar = () => {
-    const notification = document.querySelector('.notification-bar');
-    const notifcationClose = document.querySelector('.notification-bar__close');
+export default class NotificationBar {
+    notification = document.querySelector('.notification-bar');
+    notificationClose = document.querySelector('.notification-bar__close');
 
-    const notificationCookieValue = document.cookie
-        .split('; ')
-        .find((row) => row.startsWith('notification='))
-        ?.split('=')[1];
+    init = () => {
+        if (this.notification) {
+            document.body.classList.add('body--notification');
+        }
 
-    if (notification) {
-        document.body.classList.add('body--notification');
+        if (this.getNotificationCookie() === 'closed') {
+            this.removeNotification();
+        }
+
+        this.notificationClose.addEventListener('click', () => {
+            this.removeNotification();
+            this.addCookie();
+        });
     }
 
-    if (notificationCookieValue === 'closed') {
+    getNotificationCookie = () => {
+        return document.cookie
+            .split('; ')
+            .find((row) => row.startsWith('notification='))
+            ?.split('=')[1];
+    }
+    
+    removeNotification = () => {
         if (document.body.classList.contains('body--notification')) {
             document.body.classList.remove('body--notification');
         }
-
-        notification.remove();
+    
+        this.notification.remove();
     }
-
-    notifcationClose.addEventListener('click', () => {
-        if (document.body.classList.contains('body--notification')) {
-            document.body.classList.remove('body--notification');
-        }
-
-        if (notificationCookieValue === undefined || notificationCookieValue !== 'closed') {
-            const cookieExpirationDate = new Date();
-            cookieExpirationDate.setUTCDate(cookieExpirationDate.getUTCDate() + 7);
-            document.cookie = 'notification=closed; expires=' + cookieExpirationDate.toUTCString();
-        }
-
-        notification.remove();
-    });
+    
+    addCookie = () => {
+        const cookieExpirationDate = new Date();
+        cookieExpirationDate.setUTCDate(cookieExpirationDate.getUTCDate() + 7);
+        document.cookie = 'notification=closed; expires=' + cookieExpirationDate.toUTCString();
+    }
 }
-
-export default notificationBar;
