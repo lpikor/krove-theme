6b95cef25a4b9c1696e5a079e0f85133f11d23b9
Łukasz Pikor <pikorlukasz@gmail.com>
2023-04-05 07:47:13 +0200

    Improve accessibility of dropdown menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,48 @@
 export default class Menu {
     pageHeader = document.querySelector('.page-header');
     menuTriggers = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item > a');
-    menuItemParent = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item-has-children > a');
+    menuItemParents = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item-has-children > a');
+    subMenuChildren = document.querySelectorAll('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item > .sub-menu > .menu-item a');
 
     init = () => {
-        this.handleParentMouseEnter();
+        this.handleParent();
+        this.handleChildren();
         this.handleMenuTriggersMouseEnter();
         this.handlePageHeaderMouseLeave();
     }
 
-    handleParentMouseEnter = () => {
-        this.menuItemParent.forEach(parent => {
+    handleParent = () => {
+        this.menuItemParents.forEach(parent => {
             const subMenu = parent.parentNode.querySelector('.sub-menu');
 
             parent.addEventListener('mouseenter', () => {
                 subMenu.classList.add('active');
+            });
+
+            parent.addEventListener('focusin', (e) => {
+                subMenu.classList.add('active');
+            });
+
+            parent.addEventListener('focusout', (e) => {
+                subMenu.classList.add('active');
+            });
+        });
+    }
+
+    handleChildren = () => {
+        this.subMenuChildren.forEach(child => {
+            const subMenu = child.closest('.main-nav .menu-main-navigation-menu-container > .menu > .menu-item > .sub-menu');
+
+            child.addEventListener('focusout', (e) => {
+                if (subMenu.classList.contains('active') && Array.from(this.menuTriggers).includes(e.relatedTarget)) {
+                    subMenu.classList.remove('active');
+                }
+            });
+            
+            child.addEventListener('focusin', (e) => {
+                if (!subMenu.classList.contains('active') && Array.from(this.menuTriggers).includes(e.relatedTarget)) {
+                    subMenu.classList.add('active');
+                }
             });
         });
     }
